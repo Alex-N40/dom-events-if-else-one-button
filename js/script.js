d81934c406456bfacb04bb55e564eb5f01df03de
Alex-N40 <alex_n40@proton.me>
2024-04-19 15:44:04 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*if else statements & events using one button*/
+
+var darkModeButton = document.querySelector(".dark-mode");
+var theme = document.querySelector("body");
+
+darkModeButton.addEventListener("click", function () {
+  if (theme.classList.contains("dark-palette")) {
+    theme.classList.remove("dark-palette");
+    darkModeButton.innerText = "Dark Mode";
+  } else {
+    theme.classList.add("dark-palette");
+    darkModeButton.innerText = "Light Mode";
+  }
+});
